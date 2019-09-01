6f0e1aaa06ec871809d92c34786d28ebbbf10eb4
dj0nny <frank96990@gmail.com>
2019-09-01 14:43:18 +0200

    generate README.md

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ async function generateTables(repos) {
   let string = '';
   for (let i = 0; i < repos.length; ++i) {
     string += `| [**${repos[i].title}**<br/> ` +
-      `![${repos[i].title}](${repos[i].logo})` +
+      `![${repos[i].title}](${repos[i].logo})<br/>` +
       `![Star](https://img.shields.io/github/stars/${repos[i].repo}.svg?style=social&label=Star) ` +
       `![Fork](https://img.shields.io/github/forks/${repos[i].repo}.svg?style=social&label=Fork)]` +
       `(https://github.com/${repos[i].repo})`;
