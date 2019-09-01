6a8f898bf032c2cce9900cfddb264b783cf0664e
dj0nny <frank96990@gmail.com>
2019-09-01 14:13:25 +0200

    edit push.sh

--- src/push.sh
+++ src/push.sh
@@ -11,5 +11,5 @@ git branch -d $BRANCH_NAME || true
 git checkout -b $BRANCH_NAME
 git add ../README.md
 git commit --message "Auto-update README" || exit 0
-git remote add origin-$BRANCH_NAME https://${GH_TOKEN}@github.com/${GH_REPO}.git
+git remote add origin-$BRANCH_NAME https://${GITHUB_TOKEN}@github.com/${GH_REPO}.git
 git push --force --quiet --set-upstream origin-$BRANCH_NAME $BRANCH_NAME
